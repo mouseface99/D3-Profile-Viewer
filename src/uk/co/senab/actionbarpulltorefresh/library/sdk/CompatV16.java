9bc35e3a34e12dbb422d150b9737521e99855ed7
Mike_Wang <mike_wang@compal.com>
2014-07-04 16:23:41 +0800

    Fine Tune
    1. Update Launcher ICON !
    2. Fix some lint error/warning

--- src/uk/co/senab/actionbarpulltorefresh/library/sdk/CompatV16.java
+++ src/uk/co/senab/actionbarpulltorefresh/library/sdk/CompatV16.java
@@ -1,10 +1,13 @@
 package uk.co.senab.actionbarpulltorefresh.library.sdk;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.view.View;
 
 class CompatV16 {
 
-    static void postOnAnimation(View view, Runnable runnable) {
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	static void postOnAnimation(View view, Runnable runnable) {
         view.postOnAnimation(runnable);
     }
 
